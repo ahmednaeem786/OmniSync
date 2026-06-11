9a38fc09ee2b8d63edf6d696a3fe7fae110d323f
Ahmed Naeem Bhuri <ahmednaeem786@hotmail.com>
2026-06-11 14:24:04 +0500

    Service asks the user for notification permissions and then boots up the OmniSyncService. Keeps running in background even after application instance swiped away.

--- android_node/app/src/main/java/com/ahmednaeem786/omnisync/MainActivity.kt
+++ android_node/app/src/main/java/com/ahmednaeem786/omnisync/MainActivity.kt
@@ -13,10 +13,36 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ahmednaeem786.omnisync.ui.theme.OmniSyncTheme
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+
 class MainActivity : ComponentActivity() {
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            startBackgroundService()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                startBackgroundService()
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        } else {
+            startBackgroundService()
+        }
+
         setContent {
             OmniSyncTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -28,12 +54,25 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun startBackgroundService() {
+        val serviceIntent = Intent(this, OmniSyncService::class.java)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent)
+        } else {
+            startService(serviceIntent)
+        }
+    }
+
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "🫨 OmniSync Background Service Active!" +
+                "\nYou can swipe the app away, background tunnel will remain open." +
+                "\nSee you on the other side ;)",
         modifier = modifier
     )
 }
