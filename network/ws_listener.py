b7c470e31db9f6928b757fef5d03f72c800615ca
Ahmed Naeem Bhuri <ahmednaeem786@hotmail.com>
2026-07-09 00:23:04 +0500

    Added WebSocket listener for cloud clipboard sync

--- network/ws_listener.py
+++ network/ws_listener.py
@@ -1,3 +1,10 @@
+import json
+import websocket
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 """
 Job: It is completely responsible for pulling data down from the cloud.
 
@@ -5,3 +12,38 @@ Rules: You give this class a reference to your ClipboardManager.
 It opens the permanent WebSocket tunnel and goes to sleep. 
 When the cloud shouts a new copied text down the tunnel, this class catches it and immediately tells the ClipboardManager to paste it.
 """
+
+class CloudListener:
+    
+    def __init__(self, clipboard_manager):
+        self.clipboard = clipboard_manager
+        self.headers = [f"Authorization: Bearer {os.getenv('API_KEY')}"]
+
+    def _on_message(self, ws, message):
+        try:
+            data = json.loads(message)
+            text = data.get("text")
+
+            if text:
+                print(f"\n[WEBSOCKET] Recieved live clipboard data from the cloud: {text}")
+                self.clipboard.write_from_android(text)
+        
+        except Exception as e:
+            print(f"[WEBSOCKET] Error processing message: {e}")
+
+    def _on_error(self, ws, error):
+        print(f"[WEBSOCKET] Tunnel Error: {error}")
+
+    def start(self):
+
+        print(f"Opening secure WebSocket tunnel to {os.getenv('WS_URL')}")
+
+        ws = websocket.WebSocketApp(
+            
+            os.getenv('WS_URL'),
+            header=self.headers,
+            on_message=self._on_message,
+            on_error=self._on_error
+        )
+
+        ws.run_forever()
